5062c25c11f20d6ce03728aa4c649467f6788fdc
Sally Poon <sallypoon@hotmail.com>
2021-06-02 17:02:31 -0700

    Write and style home

--- my-portfolio/src/containers/Home/HomePage.js
+++ my-portfolio/src/containers/Home/HomePage.js
@@ -2,16 +2,23 @@ import React from 'react';
 
 import './HomePage.css';
 
-// Assets
-import portrait from '../../config/assets/images/me.png';
 
 const HomePage = () => {
     return (
         <div id="homepage-bg">
-        <div id="homepage-portrait"></div>
-        <p id="fname" className="homepage-typography">Sally</p>
-        <p id="lname" className="homepage-typography">Poon</p>
 
+            <div id="homepage-nav">
+                <p id="fname" className="homepage-typography">Sally </p>
+                <p id="lname" className="homepage-typography">Poon</p>
+
+
+            </div>
+
+            <ul id="hpage-nav">
+                    <li><a href="/projects">Projects</a></li>
+                    <li><a href="/playground">Playground</a></li>
+                    <li><a href="/about-sally">About</a></li>
+            </ul>
         </div>
     )
 }
