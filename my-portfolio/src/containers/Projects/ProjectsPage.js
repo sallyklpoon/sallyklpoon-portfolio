bc5bf6f1c75a0d966346cd5d23d9970cb3099503
Sally Poon <sallypoon@hotmail.com>
2021-06-12 00:24:45 -0700

    Remove placeholder gallery items

--- my-portfolio/src/containers/Projects/ProjectsPage.js
+++ my-portfolio/src/containers/Projects/ProjectsPage.js
@@ -33,54 +33,6 @@ const ProjectsPage = () => {
 
             <section>
                 <ProjectGallery projects={projects} />
-
-                <Link to="/blah">
-                    <div className="project-container" data-aos="fade-up" data-aos-duration="1000">
-                        <img src="https://i.imgur.com/Ejn9qV0.jpg" alt="" className="project-image"/>
-                        <div className="project-overlay">
-                            <h4 className="project-title">Project 1</h4>
-                            <div className="project-text">May 2021 - Present </div>
-                            <div className="view-project">View Project</div>
-                        </div>
-                    </div>
-                </Link>
-
-                <div className="project-container">
-                    <img src="https://i.imgur.com/Ejn9qV0.jpg" alt="" className="project-image"/>
-                    <div className="project-overlay">
-                        <div className="project-text">Hello World</div>
-                    </div>
-                </div>
-
-                <div className="project-container">
-                    <img src="https://i.imgur.com/Ejn9qV0.jpg" alt="" className="project-image"/>
-                    <div className="project-overlay">
-                        <p className="project-text">Hello World</p>
-                    </div>
-                </div>
-
-                <div className="project-container">
-                    <img src="https://i.imgur.com/Ejn9qV0.jpg" alt="" className="project-image"/>
-                    <div className="project-overlay">
-                        <h4 className="project-title">Project 1</h4>
-                        <div className="project-text">Hello World</div>
-                    </div>
-                </div>
-
-                <div className="project-container">
-                    <img src="https://i.imgur.com/Ejn9qV0.jpg" alt="" className="project-image"/>
-                    <div className="project-overlay">
-                        <div className="project-text">Hello World</div>
-                    </div>
-                </div>
-
-                <div className="project-container">
-                    <img src="https://i.imgur.com/Ejn9qV0.jpg" alt="" className="project-image"/>
-                    <div className="project-overlay">
-                        <p className="project-text">Hello World</p>
-
-                    </div>
-                </div>
             </section>
 
             <footer id="project-foot"></footer>
