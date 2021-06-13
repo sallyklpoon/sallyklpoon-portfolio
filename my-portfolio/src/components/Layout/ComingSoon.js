1c086c929e34e9c84cbb37d183218098411da3b9
Sally Poon <sallypoon@hotmail.com>
2021-06-13 00:02:00 -0700

    Update project images, initiate coming soon element

---
+++ my-portfolio/src/components/Layout/ComingSoon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import planty from '../../config/assets/images/planty-wave.gif';
+
+const ComingSoon = () => {
+    return (
+        <>
+        Playground! :)
+        
+        </>
+    )
+}
+
+export default ComingSoon;
