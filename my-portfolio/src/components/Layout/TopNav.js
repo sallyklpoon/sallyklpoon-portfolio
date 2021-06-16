a836e47a37e5c9d9ab6d398c40da329ec7a5da24
Sally Poon <sallypoon@hotmail.com>
2021-06-15 23:29:48 -0700

    Add toggle on mobile

--- my-portfolio/src/components/Layout/TopNav.js
+++ my-portfolio/src/components/Layout/TopNav.js
@@ -6,14 +6,16 @@ import Nav from "react-bootstrap/Nav";
 
 const TopNav = () => {
     return (
-        <Navbar collapseOnSelect fixed="top" id="top-nav">
+        <Navbar collapseOnSelect expand="lg" fixed="top" id="top-nav">
             <Navbar.Brand href="/">Sally Poon</Navbar.Brand>
-        
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
             <Nav.Link href="/#projects" className="mr-3">Projects</Nav.Link>
-            <Nav.Link href="playground" className="mr-3">Playground</Nav.Link>
+            <Nav.Link href="playground" className="mr-3">Play</Nav.Link>
             <Nav.Link href="/#about" className="mr-3">About</Nav.Link>
         </Nav>
+        </Navbar.Collapse>
         </Navbar>
     )
 }
