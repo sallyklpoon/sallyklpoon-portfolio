23892ca7ebeb3cda4bd9caf3e1b20118a42759d8
Sally Poon <sallypoon@hotmail.com>
2021-09-01 00:55:46 -0700

    Replace sprout dummy info with useParam()

--- src/containers/Projects/ProjectDetailsPage.js
+++ src/containers/Projects/ProjectDetailsPage.js
@@ -23,8 +23,20 @@ const ProjectDetailsPage = () => {
 
     const [, setOffsetY] = useState(0);
     const handleScroll = () => setOffsetY(window.pageYOffset);
-    const htmlIntro = data.sprout.description;
-    const htmlRetrospective = data.sprout.retrospective;
+    let projectParam = useParams().project_name
+    const htmlIntro = data[projectParam].description;
+    const htmlRetrospective = data[projectParam].retrospective;
+
+    console.log()
+    
+    const heroStyle = {
+        backgroundImage: `url(${data[projectParam].hero})`,
+        height: '40vh',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        boxShadow:'inset 0 -20px 10px #3844282c'
+    }
 
     useEffect (() => {
         window.scrollTo(0, 0)
@@ -33,54 +45,41 @@ const ProjectDetailsPage = () => {
         return () => window.removeEventListener('scroll', handleScroll);
     }, [])
 
-    const heroStyle = {
-        backgroundImage: `url(${data.sprout.hero})`,
-        height: '40vh',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        boxShadow:'inset 0 -20px 10px #3844282c'
-    }
 
     return (
+        
         <>
             <div id="capwise-hero" style={heroStyle}></div>
             
             <div id="project-page-container">
 
-                <h1 className="display-1" id="top">{data.sprout.name}</h1>
+                <h1 className="display-1" id="top">{data[projectParam].name}</h1>
                 <p className="lead project-roles"> 
-                    <strong>My Roles: </strong>{data.sprout.roles}
+                    <strong>My Roles: </strong>{data[projectParam].roles}
                 </p>
 
                 <div className="project-section">
                     <p dangerouslySetInnerHTML={ {__html: htmlIntro} }/>
 
-                    {/**buttons here */}
-                    <ProjectLinks   url={data.sprout.url} 
-                                    github={data.sprout.github} 
-                                    trello={data.sprout.trello} 
-                                    devpost={data.sprout.devpost} 
-                                    miro={data.sprout.miro} />
+                    <ProjectLinks url={data[projectParam].url} 
+                                github={data[projectParam].github} 
+                                trello={data[projectParam].trello} 
+                                devpost={data[projectParam].devpost} 
+                                miro={data[projectParam].miro} />
 
                     <div className="responsive-player-container">
-                        <ResponsivePlayer video_url={data.sprout.pitch_url}/>
+                        <ResponsivePlayer video_url={data[projectParam].pitch_url}/>
                     </div>
                 </div>
 
                 <div className="project-section">
-
                     <hr/>
                     <h2 className="display-2">Tech Stack</h2>
-                    <img className="full-img" src={data.sprout.techstack_img} alt="techstack-capwise" data-aos="fade-up" data-aos-duration="2000"/>
+                    <img className="full-img" src={data[projectParam].techstack_img} alt="techstack-capwise" data-aos="fade-up" data-aos-duration="2000"/>
 
                     <ul className="columns-2" data-aos="fade-down" data-aos-duration="1000">
-                        <div>
-                            <BulletContent bullets={data.sprout.techstack_c1}/>
-                        </div>
-                        <div>
-                            <BulletContent bullets={data.sprout.techstack_c2}/>
-                        </div>
+                        <div><BulletContent bullets={data[projectParam].techstack_c1}/></div>
+                        <div><BulletContent bullets={data[projectParam].techstack_c2}/></div>
                     </ul>
                 </div>
 
@@ -88,13 +87,13 @@ const ProjectDetailsPage = () => {
                 <div className="project-section">
                     <h2 className="display-2">Retrospective</h2>
                     <p dangerouslySetInnerHTML={ {__html: htmlRetrospective} }/>
-                    <DoubleAccordion content={data.sprout}/>
-                    <ProjectImages images={data.sprout.project_imgs}/>
+                    <DoubleAccordion content={data[projectParam]}/>
+                    <ProjectImages images={data[projectParam].project_imgs}/>
                     <hr/>
                 </div>
         
-                <Link to={data.sprout.nextproj_url}>
-                    <img src={data.sprout.nextproj_img} alt="next-project-capwise" className="full-img next-project"></img>
+                <Link to={data[projectParam].nextproj_url}>
+                    <img src={data[projectParam].nextproj_img} alt="next-project-capwise" className="full-img next-project"></img>
                 </Link>
 
                 <div id="project-page-btm_block"></div>
