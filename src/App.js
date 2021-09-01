ba5526726e6661fe4393d5f89b477146e5aaf69b
Sally Poon <sallypoon@hotmail.com>
2021-09-01 01:17:01 -0700

    Remove redundant/old files

--- src/App.js
+++ src/App.js
@@ -16,16 +16,7 @@ import TopNav from "./components/Layout/TopNav";
 
 // Page Containers
 import HomePage from './containers/Home/HomePage';
-// import ProjectsPage from './containers/Projects/ProjectsPage';
 import PlaygroundPage from './containers/Playground/PlaygroundPage';
-import AboutPage from './containers/AboutMe/AboutMe';
-
-// Project Pages
-import SproutPage from './containers/Projects/Sprout/SproutPage';
-import CapWisePage from './containers/Projects/CapWise/CapWisePage';
-import NamuPage from './containers/Projects/Namu/NamuPage';
-import BoriyaPage from './containers/Projects/Boriya/BoriyaPage';
-import OlaPage from './containers/Projects/Ola/OlaPage';
 import ProjectDetailsPage from './containers/Projects/ProjectDetailsPage';
 
 
@@ -42,14 +33,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          {/* <Route path="/projects" component={ProjectsPage} /> */}
           <Route path="/playground" component={PlaygroundPage} />
-          <Route path="/about-sally" component={AboutPage} />
-          <Route path="/sprout" component={SproutPage} />
-          <Route path="/capwise" component={CapWisePage} />
-          <Route path="/namu" component={NamuPage} />
-          <Route path="/boriya" component={BoriyaPage} />
-          <Route path="/ola-chat" component={OlaPage} />
           <Route path="/project/:project_name" component={ProjectDetailsPage}/>
         </Switch>
       </Router>
